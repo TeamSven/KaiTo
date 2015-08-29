eacc029f6853149f124870ee00e8ac1bbc6f8991
dnivra26 <dnivra26@gmail.com>
2015-08-29 18:37:13 +0530

    Added parse configuration

---
+++ app/src/main/java/io/github/dnivra26/kaito/KaiToApplication.java
@@ -0,0 +1,23 @@
+package io.github.dnivra26.kaito;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseUser;
+
+public class KaiToApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(this, "UpGUiLqb4Kz2IbuNGRxloKbEnZmR6aUExSSucmSd", "B3oYsXxVZoBaqw3VQPSodhO2VuOrVnmAvCOoxwBu");
+
+        ParseUser.enableAutomaticUser();
+        ParseACL defaultACL = new ParseACL();
+
+        defaultACL.setPublicReadAccess(true);
+        defaultACL.setPublicWriteAccess(true);
+
+        ParseACL.setDefaultACL(defaultACL, true);
+    }
+}
